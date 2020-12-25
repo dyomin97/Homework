7dc2ba0a97c69bff0be41573d8e3b9fc21cca1b8
Evgenii Dyomin <e.dyomin97@gmail.com>
2020-12-25 17:05:16 +0500

    Updated

--- 23.12/index.php
+++ 23.12/index.php
@@ -13,17 +13,35 @@ foreach($rand_array as $value){
         echo "$value <br/>";
     }
 }
+
+echo '<hr />';
 //Task 2
 
 $arr2 = array('Африка' => array('Алжир', 'Танзания','Тунис'),
-         'Южная Америка' => array('Бразилия','Аргентина', 'Колумбия'));
+    'Южная Америка' => array('Бразилия','Аргентина', 'Колумбия'));
 foreach($arr2 as $key=>$value) {
     foreach ($value as $value2) {
-if(strpos($value2, 'А') === 0){
-echo $value2;
+        if(strpos($value2, 'А') === 0){
+            echo $value2.' ';
+        }
     }
 }
+echo "<hr/>";
+//Task 3
+$arr3 =array(array(1,5,10,35,40), array(53,27,99,105,33,74,31,13));
+$sum = 0;
+foreach($arr3 as $key=>$value){
+    if($key===0) {
+        echo "First array: ";
+    } else if($key === 1){
+        echo "Second array: ";
+    }
+    echo count($value)."<br/>";
+    $sum+=count($value);
 }
+
+echo "All elements: ";
+echo $sum."<br/>";
 ?>
 
 
